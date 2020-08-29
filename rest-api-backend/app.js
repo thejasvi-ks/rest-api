e4804850200b081395d014d12193bd92820a991f
tks <tks@stats.com>
2020-08-29 22:27:57 +0530

    Added REST API Bakckend and Front End.

---
+++ rest-api-backend/app.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const moongoose = require('mongoose');
+const path = require('path');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4() + '-' + file.originalname)
+    }
+});
+
+const fileFilter = (req,file,cb) => {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg"
+    ) {
+        cb(null, true);
+    }
+    else{
+        cb(null, false);
+    }
+}
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images',express.static(path.join(__dirname,'images')))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error,req,res,next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({
+        message: message,
+        data: data
+    });
+});
+
+
+moongoose
+  .connect(MONGO_DB_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
